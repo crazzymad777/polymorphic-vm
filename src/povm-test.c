782fd98b7c51cb3f9d7d1b889a9dafeeee01dee9
Yury Moskov <aerobus.778@gmail.com>
2024-08-27 18:42:31 +0300

    test_command_add_f64

--- src/povm-test.c
+++ src/povm-test.c
@@ -18,9 +18,26 @@ int test_command_add_i64(int64_t a, int64_t b) {
     fclose(fd);
 }
 
+int test_command_add_f64(double a, double b) {
+    const char buffer[] = {COMMAND_ADD};
+    FILE* fd = fmemopen((void*)buffer, sizeof(buffer), "r");
+    enum datum_type types[] = {F64, F64};
+    double stack[] = {a, b};
+
+    int32_t* types_ptr = (int32_t*)&types;
+    union udatum* udatum_stack = (union udatum*)stack;
+    udatum_stack += 1;
+    types_ptr += 1;
+
+    povm_execute(fd, udatum_stack, types_ptr);
+    munit_assert_int(stack[0], ==, a + b);
+    fclose(fd);
+}
+
 int main() {
     // We must test each command
     // We must test generator
     test_command_add_i64(25, 42);
+    test_command_add_f64(-49.0, 44.3);
     return 0;
 }
