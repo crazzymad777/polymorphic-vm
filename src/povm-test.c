2e549d58c97caeca460630cdc745fdbca6c72f01
Yury Moskov <aerobus.778@gmail.com>
2024-08-27 18:59:34 +0300

    munit test

--- src/povm-test.c
+++ src/povm-test.c
@@ -30,14 +30,42 @@ int test_command_add_f64(double a, double b) {
     types_ptr += 1;
 
     povm_execute(fd, udatum_stack, types_ptr);
-    munit_assert_int(stack[0], ==, a + b);
+    munit_assert_double(stack[0], ==, a + b);
     fclose(fd);
 }
 
-int main() {
-    // We must test each command
-    // We must test generator
+static MunitResult test_command_add(const MunitParameter params[], void* user_data) {
     test_command_add_i64(25, 42);
     test_command_add_f64(-49.0, 44.3);
-    return 0;
+    return MUNIT_OK;
 }
+
+static MunitTest test_suite_tests[] = {
+  {
+    (char*) "test_command_add",
+    test_command_add,
+    NULL,
+    NULL,
+    MUNIT_TEST_OPTION_NONE,
+    NULL
+  },
+  { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
+};
+
+static const MunitSuite test_suite = {
+  (char*) "",
+  test_suite_tests,
+  NULL,
+  1,
+  MUNIT_SUITE_OPTION_NONE
+};
+
+#include <stdlib.h>
+
+int main(int argc, char* argv[MUNIT_ARRAY_PARAM(argc + 1)]) {
+  /* Finally, we'll actually run our test suite!  That second argument
+   * is the user_data parameter which will be passed either to the
+   * test or (if provided) the fixture setup function. */
+  return munit_suite_main(&test_suite, (void*) "povm-test", argc, argv);
+}
+
